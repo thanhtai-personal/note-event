270f250dbdfc9966aff5d4dd6cd59b323965c746
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2021-03-21 16:49:12 +0700

    update database inittor for Mac

--- backend/database/dev/pool.js
+++ backend/database/dev/pool.js
@@ -2,11 +2,9 @@
 
 import { Pool } from 'pg';
 
-import dotenv from 'dotenv';
+import env from './../../env';
 
-dotenv.config();
-
-const databaseConfig = { connectionString: process.env.DEV_DATABASE_URL };
+const databaseConfig = { connectionString: env.database_url };
 const pool = new Pool(databaseConfig);
 
 export default pool;
