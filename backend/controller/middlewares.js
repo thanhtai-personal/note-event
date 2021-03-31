a4bd909d88143bda9867c8e45e5cb22fd1b90adb
Tai Tran <tatran@deloitte.ca>
2021-03-31 09:46:49 +0700

    add use Auth middle wares

--- backend/controller/middlewares.js
+++ backend/controller/middlewares.js
@@ -1,7 +1,22 @@
 const publicRoute = [ 'login', 'register' ]
+const { jwtKey } = require('./../env')
+const jwt = require('jsonwebtoken')
+
+const checkValidateToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, jwtKey)
+    return decoded
+  } catch (error) {
+    return false
+  }
+}
 
 const useAuth = (req, res, next) => {
   if (publicRoute.includes(req?.url)) return next() //validate token info
-  else if ('valid token') return next()
+  else if (checkValidateToken(req.headers.token)) return next()
   else return '403 error'
+}
+
+module.exports = {
+  useAuth
 }
