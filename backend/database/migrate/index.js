b342be6979c8ef9e36c385d8652a288fdcf49992
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2021-04-05 20:49:16 +0700

    update db script

--- backend/database/migrate/index.js
+++ backend/database/migrate/index.js
@@ -1,10 +1,12 @@
 const migrate240321 = require('./migrate240321')
 const migrate250321 = require('./migrate250321')
 const migrate290321 = require('./migrate290321')
+const migrate050421 = require('./migrate050421')
 module.exports =  {
   excute: async (pool) => {
     await migrate240321(pool)
     await migrate250321(pool)
-    await migrate290321(pool, true)
+    await migrate290321(pool)
+    await migrate050421(pool, true)
   }
 }
