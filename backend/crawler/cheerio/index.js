50a3ad9ac06e7487995934cea0b360ad2cdb8855
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2021-05-22 12:35:25 +0700

    add folder struct

--- backend/crawler/cheerio/index.js
+++ backend/crawler/cheerio/index.js
@@ -5,6 +5,7 @@ const excuteCherrio = async () => {
     let novals = await crawlTruyenConvert.getNovalsSummaryInfo()
     novals = await crawlTruyenConvert.getNovalsDetail(novals)
     console.log('------------FINISH!!!--------------')
+    return novals
   } catch (error) {
     console.log('------------CHERRIO FAILED ACTION!!!--------------')
     throw error
