444edf49c7e8f0192add6848fdde32027e9c04cf
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2021-06-04 20:59:16 +0700

    update header cors

--- frontend/src/api/prodConfig.js
+++ frontend/src/api/prodConfig.js
@@ -43,7 +43,8 @@ export default {
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://note-event-etlef.ondigitalocean.app',
+    'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   },
  
   // `params` are the URL parameters to be sent with the request
